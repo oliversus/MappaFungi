dad207e6049ddb99612e8bba010cf5e1fcb99ed7
Oliver Sus <osus79@gmail.com>
2016-08-16 11:49:16 +0200

    replaced switch button with ListView of available fungi

--- java/sus/oliver/mappafungi/HomeActivity.java
+++ java/sus/oliver/mappafungi/HomeActivity.java
@@ -2,12 +2,21 @@ package sus.oliver.mappafungi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    String fungiTypes[] = {"Cep", "Chanterelle", "Morel"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, fungiTypes);
+
+        ListView fungiListView = (ListView)findViewById(R.id.fungiListView);
+        fungiListView.setAdapter(adapter);
     }
 }
