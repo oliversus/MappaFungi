2c1b52912818e8f9f5d578437ebbd8792203e746
Oliver Sus <osus79@gmail.com>
2016-08-16 11:06:10 +0200

    added home activity, showing list of mushrooms to select

--- java/sus/oliver/mappafungi/MainActivity.java
+++ java/sus/oliver/mappafungi/MainActivity.java
@@ -1,7 +1,10 @@
 package sus.oliver.mappafungi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView logoImage = (ImageView)findViewById(R.id.image_logo);
+        logoImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Mappa Fungi");
+                loadHomeActivity();
+            }
+        });
+
+    }
+
+    private void loadHomeActivity(){
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
